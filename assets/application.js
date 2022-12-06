d42e7973d249aa18064195419e7c2b8985a1a6a4
core.editor <miriam@directimpact.online>
2022-12-06 16:37:05 +0100

    Fix JS: Sort by

--- assets/application.js
+++ assets/application.js
@@ -1,6 +1,8 @@
-document.querySelector("#sort_by").addEventListener("change", function (e) {
-  var url = new URL(window.location.href);
-  url.searchParams.set("sort_by", e.currentTarget.value);
+if (document.getElementById("sort_by") != null) {
+  document.querySelector("#sort_by").addEventListener("change", function (e) {
+    var url = new URL(window.location.href);
+    url.searchParams.set("sort_by", e.currentTarget.value);
 
-  window.location = url.href;
-});
+    window.location = url.href;
+  });
+}
