3193a2af32a5a5ff2e0a0fda390f4a52314affc6
core.editor <miriam@directimpact.online>
2022-12-19 11:14:56 +0100

    Addresses

--- assets/application.js
+++ assets/application.js
@@ -6,3 +6,32 @@ if (document.getElementById("sort_by") != null) {
     window.location = url.href;
   });
 }
+
+if (document.getElementById("AddressCountryNew") != null) {
+  document
+    .getElementById("AddressCountryNew")
+    .addEventListener("change", function (e) {
+      var provinces =
+        this.options[this.selectedIndex].getAttribute("data-provinces");
+      var provincesSelector = document.getElementById("AddressProvinceNew");
+      var provincesArray = JSON.parse(provinces);
+      console.log(provincesArray);
+
+      if (provincesArray.length < 1) {
+        provincesSelector.setAttribute("disabled", "disabled");
+      } else {
+        provincesSelector.removeAttribute("disabled");
+      }
+      provincesSelector.innerHTML = " ";
+      var options = " ";
+      for (var i = 0; i < provincesArray.length; i++) {
+        options +=
+          '<option value="' +
+          provincesArray[i][0] +
+          '">' +
+          provincesArray[i][0] +
+          "</option>";
+      }
+      provincesSelector.innerHTML = options;
+    });
+}
